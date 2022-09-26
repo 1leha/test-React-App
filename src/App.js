f5399f6d1e25b0f9127b319443747ae566ece95b
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-09-26 10:55:57 +0400

    sec commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!fsdfsdf
+          Learn React_LAP
         </a>
       </header>
     </div>
